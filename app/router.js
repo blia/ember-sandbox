1fa70367589663964d7975e9344da45b2162788e
Kirill Yakovenko <kirill.yakovenko@me.com>
2015-06-02 16:49:53 +0300

    Вывщдим один пост

--- app/router.js
+++ app/router.js
@@ -7,6 +7,6 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
   this.route('posts', { path: '/' }, function () {
-    this.route('post', { path: ':id' })
+    this.resource('post', { path: ':id' })
   })
 });
