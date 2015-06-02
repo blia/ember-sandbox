e304586bdfa1ca69bd949e392df8684aafc52905
Kirill Yakovenko <kirill.yakovenko@me.com>
2015-06-02 16:09:58 +0300

    Создаем http mock
    `ember g http-mock posts`
    
    В ембер есть встроенный сервер для разработки и комманды для генерировая
    моком и прокси. Сейчас мы создали REST api для ресурса post
    Нужно перегрузить ембер сервер

---
+++ server/mocks/posts.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': []
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
